7119983b59fbcecb669f44d1b190ac570d97f6f4
tuminskiy <unknowuzvername@gmail.com>
2021-02-26 23:52:48 +0300

    Add class TcpServer

---
+++ server/tcpserver/tcpserver.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <QTcpServer>
+
+#include <list>
+
+class QTcpSocket;
+
+namespace storage { class Database; }
+
+namespace network {
+
+class TcpServer : public QObject
+{
+  Q_OBJECT
+
+  storage::Database* db_;
+  QTcpServer server_;
+  std::list<QTcpSocket*> connections_;
+
+public:
+  explicit TcpServer(storage::Database* db, QObject* parent = nullptr);
+
+  bool listen(quint16 port);
+
+private slots:
+  void new_connection();
+  void read_header();
+
+  void reg_request(QTcpSocket* sender, const QByteArray& bytes);
+  void auth_request(QTcpSocket* sender, const QByteArray& bytes);
+  void msg_request(QTcpSocket* sender, const QByteArray& bytes);
+};
+
+} // namespace network
