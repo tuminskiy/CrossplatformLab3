8d3f2ee641ca200e824ab4b74e945f02c84f3bd0
tuminskiy <unknowuzvername@gmail.com>
2021-02-27 02:29:52 +0300

    Add impl MainWindow

--- client/gui/mainwindow/mainwindow.h
+++ client/gui/mainwindow/mainwindow.h
@@ -1,17 +1,33 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QStringListModel>
 
 #include "ui_mainwindow.h"
+
+class QTcpSocket;
 
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
 
+  Ui::MainWindow ui_;
+  QTcpSocket* socket_;
+  QString login_;
+  QStringListModel model_;
+
 public:
-  explicit MainWindow(QWidget *parent = nullptr);
-  ~MainWindow() = default;
+  explicit MainWindow(QTcpSocket* socket, QWidget *parent = nullptr);
+
+  void set_login(const QString& login);
+
+private slots:
+  void msg_change();
+
+  void send_click();
+
+  void read_response();
 
 private:
-  Ui::MainWindow ui_;
+  void append_msg(const QString& msg);
 };
