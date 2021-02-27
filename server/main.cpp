ca58d11cbdbc364569a5fcf3b71e7958db373368
tuminskiy <unknowuzvername@gmail.com>
2021-02-27 03:30:07 +0300

    Take database configuration from ini file

--- server/main.cpp
+++ server/main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <QSettings>
 
 #include <iostream>
 
@@ -6,31 +7,63 @@
 #include "tcpserver/tcpserver.h"
 #include "storage/database.h"
 
+bool is_valid_settings(QSettings& settings);
+
 int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
 
+  if (argc != 2) {
+    std::cout << "Database configuration file was not transferred\n";
+    return 0;
+  }
+
+  QSettings settings(argv[1], QSettings::IniFormat);
+
+  if (!is_valid_settings(settings)) {
+    std::cout << "Incorrect config file\n";
+    return 0;
+  }
+
   storage::DatabaseConfig config;
-  config.type = "QPSQL7";
-  config.hostname = "127.0.0.1";
-  config.port = 5432;
-  config.db_name = "tumin";
-  config.username = "tumin";
-  config.password = "1234";
+  settings.beginGroup("DatabaseConfig");
+  config.type = settings.value("type").toString();
+  config.hostname = settings.value("hostname").toString();
+  config.port = settings.value("port").toInt();
+  config.db_name = settings.value("db_name").toString();
+  config.username = settings.value("username").toString();
+  config.password = settings.value("password").toString();
+  settings.endGroup();
 
   storage::Database db(config);
 
   if (!db.open()) {
-    std::cout << "Failed connect to database" << std::endl;
+    std::cout << "Failed connect to database\n";
     return 0;
   }
 
   network::TcpServer server(&db);
 
   if (!server.listen(protocol::SERVER_DEFAULT_PORT)) {
-    std::cout << "Failed start server on port: " << protocol::SERVER_DEFAULT_PORT << std::endl;
+    std::cout << "Failed start server on port: " << protocol::SERVER_DEFAULT_PORT << '\n';
     return 0;
   }
 
   return a.exec();
+}
+
+bool is_valid_settings(QSettings& settings)
+{
+  bool result = true;
+
+  settings.beginGroup("DatabaseConfig");
+  result &= settings.contains("type");
+  result &= settings.contains("hostname");
+  result &= settings.contains("port");
+  result &= settings.contains("db_name");
+  result &= settings.contains("username");
+  result &= settings.contains("password");
+  settings.endGroup();
+
+  return result;
 }
